91f2b711eb5618df93997834227c72daad043f60
Nikita Kulchytskyi <zpaulz61@gmail.com>
2024-09-05 01:31:14 +0200

    Error component

---
+++ src/enums/error-messages.ts
@@ -0,0 +1,7 @@
+export enum ErrorMessage {
+  Default = "Oops, something went wrong. Try again later.",
+  NotFound = "Information about the cocktail is TOP secret",
+  Unauthorized = "You are not authorized to view this content.",
+  ServerError = "A server error occurred. Please try again later.",
+  NetworkError = "A network error occurred. Please check your connection.",
+}
