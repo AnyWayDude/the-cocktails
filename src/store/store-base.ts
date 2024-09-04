1493a857a260709c7041c7f317e91287cfc36793
Nikita Kulchytskyi <zpaulz61@gmail.com>
2024-09-05 01:28:29 +0200

    add new store

--- src/store/store-base.ts
+++ src/store/store-base.ts
@@ -6,10 +6,12 @@ import {
 import { configureStore } from "@reduxjs/toolkit";
 import { config as Config } from "../config/config.js";
 import { reducer as cocktailsReducer } from "./cocktails/cocktails.js";
+import { reducer as cocktailDetailsReducer } from "./cocktail-details/coctail-details.js";
 import { http } from "../http/http.js";
 
 type RootReducer = {
   cocktails: ReturnType<typeof cocktailsReducer>;
+  cocktailDetails: ReturnType<typeof cocktailDetailsReducer>;
 };
 
 type ExtraArguments = {
@@ -30,6 +32,7 @@ class Store {
       devTools: config.ENV.APP.ENVIRONMENT !== "production",
       reducer: {
         cocktails: cocktailsReducer,
+        cocktailDetails: cocktailDetailsReducer,
       },
       middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware({
