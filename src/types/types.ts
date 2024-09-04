47a1f52592f6a5a0351e07511882006e4e7b6eb5
Nikita Kulchytskyi <zpaulz61@gmail.com>
2024-09-04 11:15:03 +0200

    new types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,3 @@
 export { type AsyncThunkConfig } from "./async-thunk-config.type.js";
+export { type Cocktail } from "./cocktail.type.js";
+export { type LoadAllResponse } from "./api/coctail-response.type.js";
