38d1fd2959360426b4f25c65b7cf9b64db84badb
Nikita Kulchytskyi <zpaulz61@gmail.com>
2024-09-05 01:25:15 +0200

    router

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import OrdinaryPage from "./OrdinaryPage";
+import OrdinaryPage from "../pages/OrdinaryPage";
+import CocktailDetailsPage from "../pages/CoctailDetailsPage";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<OrdinaryPage />} />
-      <Route path="/drink/:id" />
+      <Route path="/cocktail/:id" element={<CocktailDetailsPage />} />
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./AppRouter.js";
-import Header from "./fixed/Header.js";
+import AppRouter from "../components/AppRouter.js";
+import Header from "../components/Header.js";
 
 function App() {
   return (
