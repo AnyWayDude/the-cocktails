9b050fe9b18e719f0e59f0415f91f0e9030ebea6
Nikita Kulchytskyi <zpaulz61@gmail.com>
2024-09-05 01:34:04 +0200

    Set cocktail id in store

--- src/components/MediaCardWrapper.tsx
+++ src/components/MediaCardWrapper.tsx
@@ -1,19 +1,20 @@
-import { useNavigate } from 'react-router-dom'
-import MediaCard from './MediaCard'
-import { useAppDispatch } from '../hooks/use-app-dispatch.hook'
-import { loadCocktailDetails } from '../store/cocktail-details/actions'
-import { CocktailDetails } from '../types/cocktail-details.type'
+import { useNavigate } from "react-router-dom";
+import MediaCard from "./MediaCard.js";
+import { useAppDispatch } from "../hooks/use-app-dispatch.hook.js";
+import { actions as cocktailDetailsActions } from "../store/cocktail-details/coctail-details.js";
+
+import { Cocktail } from "../types/cocktail.type.ts";
 
 type Props = {
-    cocktail: CocktailDetails
-}
+  cocktail: Cocktail;
+};
 
 export default function MediaCardWrapper({ cocktail }: Props) {
-    const navigate = useNavigate()
-    const dispatch = useAppDispatch();
-    const onCardClick = () => {
-        dispatch(loadCocktailDetails.setCurrentId(cocktail.idDrink));
-        navigate(`/cocktail/${cocktail.idDrink}`)
-    }
-  return <MediaCard onClick={onCardClick} cocktail={cocktail} />
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const onCardClick = () => {
+    dispatch(cocktailDetailsActions.loadCocktailDetails(cocktail.idDrink));
+    navigate(`/cocktail/${cocktail.idDrink}`);
+  };
+  return <MediaCard onClick={onCardClick} cocktail={cocktail} />;
 }
